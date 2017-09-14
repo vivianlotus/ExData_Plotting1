920075ad4b19742dfc0137b25e8b97eaf8edd9f6
vivianlotus <31488849+vivianlotus@users.noreply.github.com>
2017-09-14 11:38:17 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,9 @@
-  hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
-  
-  dev.copy(png,"plot1.png", width=480, height=480)
-  dev.off()
+source('load_data.R')
+
+data <- loadData()
+
+png(filename='plot1.png', width=480, height=480, units='px')
+
+hist(data$Global_active_power, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
+
+dev.off()
