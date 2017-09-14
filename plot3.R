a63ac153580c005a166f1f6b0356c01a4ee9ed2b
vivianlotus <31488849+vivianlotus@users.noreply.github.com>
2017-09-14 11:39:36 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,10 +1,27 @@
-with(t, {
-    plot(Sub_metering_1~dateTime, type="l",
-         ylab="Global Active Power (kilowatts)", xlab="")
-    lines(Sub_metering_2~dateTime,col='Red')
-    lines(Sub_metering_3~dateTime,col='Blue')
-  })
-  legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
-         c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-dev.copy(png, file="plot3.png", height=480, width=480)
+source('load_data.R')
+
+data <- loadData()
+
+png(filename='plot3.png', width=480, height=480, units='px')
+
+startDay = as.POSIXct(strftime("2007-02-01 00:00:00"))
+endDay = as.POSIXct(strftime("2007-02-03 00:00:00"))
+plot(data$Time,
+     data$Sub_metering_1,
+     xlim=c(startDay, endDay),
+     xaxt="n",
+     type="l",
+     xlab="",
+     ylab="Energy sub metering"
+)
+
+lines(data$Time, data$Sub_metering_2, col="red")
+lines(data$Time, data$Sub_metering_3, col="blue")
+
+axis.POSIXct(1, at=seq(startDay, endDay, by="day"), format="%a")
+
+legend("topright", 
+       legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
+       col=c('black', 'red', 'blue'), lty=c(1, 1, 1))
+
 dev.off()
