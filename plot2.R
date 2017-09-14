7a2a6ab90da1fb44d55c4bd952a1f42be64c3f34
vivianlotus <31488849+vivianlotus@users.noreply.github.com>
2017-09-14 11:38:59 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,19 @@
- plot(t$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
- dev.copy(png,"plot2.png", width=480, height=480)
- dev.off()
+source('load_data.R')
+
+data <- loadData()
+
+png(filename='plot2.png', width=480, height=480, units='px')
+
+startDay = as.POSIXct(strftime("2007-02-01 00:00:00"))
+endDay = as.POSIXct(strftime("2007-02-03 00:00:00"))
+plot(data$Time,
+     data$Global_active_power,
+     xlim=c(startDay, endDay),
+     xaxt="n",
+     type="l",
+     xlab="",
+     ylab="Global Active Power (kilowatts)"
+     )
+axis.POSIXct(1, at=seq(startDay, endDay, by="day"), format="%a")
+
+dev.off()
