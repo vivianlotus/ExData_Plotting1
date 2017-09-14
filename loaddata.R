7f99952ce9bcd61535ff86aefc21bda322d993f1
vivianlotus <31488849+vivianlotus@users.noreply.github.com>
2017-09-14 11:37:14 -0400

    Update loaddata.R

--- loaddata.R
+++ loaddata.R
@@ -1,9 +1,9 @@
-t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
-t$Date <- as.Date(t$Date, "%d/%m/%Y")
-t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
-t <- t[complete.cases(t),]
-dateTime <- paste(t$Date, t$Time)
-dateTime <- setNames(dateTime, "DateTime")
-t <- t[ ,!(names(t) %in% c("Date","Time"))]
-t <- cbind(dateTime, t)
-t$dateTime <- as.POSIXct(dateTime)
+library(dplyr)
+
+loadData <- function() {
+  read.table('data.txt', sep=';', header=TRUE) %>%
+    mutate(Date = as.Date(Date, '%d/%m/%Y')) %>%
+    mutate(Time = as.POSIXct(strptime(paste(Date, ' ', Time), '%Y-%m-%d %H:%M:%S'))) %>%
+    filter(Time >= strftime('2007-02-01 00:00:00'), Time < strftime('2007-02-03 00:00:00')) %>%
+    tbl_df
+}
