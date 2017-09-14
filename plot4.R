b4e33f8a6deaec8a77599bcdef597236e4b0800f
vivianlotus <31488849+vivianlotus@users.noreply.github.com>
2017-09-14 11:40:21 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,18 +1,60 @@
-  par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
-  with(t, {
-    plot(Global_active_power~dateTime, type="l", 
-         ylab="Global Active Power (kilowatts)", xlab="")
-    plot(Voltage~dateTime, type="l", 
-         ylab="Voltage (volt)", xlab="")
-    plot(Sub_metering_1~dateTime, type="l", 
-         ylab="Global Active Power (kilowatts)", xlab="")
-    lines(Sub_metering_2~dateTime,col='Red')
-    lines(Sub_metering_3~dateTime,col='Blue')
-    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-    plot(Global_reactive_power~dateTime, type="l", 
-         ylab="Global Rective Power (kilowatts)",xlab="")
-  })
-  dev.copy(png, file="plot4.png", height=480, width=480)
-  dev.off()
-  
+ource('load_data.R')
+
+data <- loadData()
+
+png(filename='plot4.png', width=480, height=480, units='px')
+
+par(mfrow=c(2,2))
+
+startDay = as.POSIXct(strftime("2007-02-01 00:00:00"))
+endDay = as.POSIXct(strftime("2007-02-03 00:00:00"))
+
+plot(data$Time,
+     data$Global_active_power,
+     xlim=c(startDay, endDay),
+     xaxt="n",
+     type="l",
+     xlab="",
+     ylab="Global Active Power"
+)
+axis.POSIXct(1, at=seq(startDay, endDay, by="day"), format="%a")
+
+plot(data$Time,
+     data$Voltage,
+     xlim=c(startDay, endDay),
+     xaxt="n",
+     type="l",
+     xlab="datetime",
+     ylab="Voltage"
+)
+axis.POSIXct(1, at=seq(startDay, endDay, by="day"), format="%a")
+
+plot(data$Time,
+     data$Sub_metering_1,
+     xlim=c(startDay, endDay),
+     xaxt="n",
+     type="l",
+     xlab="",
+     ylab="Energy sub metering"
+)
+
+lines(data$Time, data$Sub_metering_2, col="red")
+lines(data$Time, data$Sub_metering_3, col="blue")
+
+axis.POSIXct(1, at=seq(startDay, endDay, by="day"), format="%a")
+
+legend("topright", 
+       legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
+       col=c('black', 'red', 'blue'), lty=c(1, 1, 1))
+
+plot(data$Time,
+     data$Global_reactive_power,
+     xlim=c(startDay, endDay),
+     xaxt="n",
+     type="l",
+     xlab="datetime",
+     ylab="Global_reactive_power"
+)
+axis.POSIXct(1, at=seq(startDay, endDay, by="day"), format="%a")
+
+dev.off()
